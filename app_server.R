f29b827120df077eab7df6059e155dd701195032
ShiraZur <szur@uw.edu>
2022-11-29 20:54:40 -0800

    created files

---
+++ app_server.R
@@ -0,0 +1,7 @@
+data <- read.csv("https://raw.githubusercontent.com/owid/co2-data/master/owid-co2-data.csv")
+
+# Calculate 3 values:
+
+# What is the average value of my variable across all the counties (in the current year)?
+# Where is my variable the highest / lowest?
+# How much has my variable change over the last N years?
